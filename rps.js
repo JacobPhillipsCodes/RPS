4603a8bbba70f9116c762f1ba39d833cb8bd7dce
Jacob Phillips <jacobphillipscodes@gmail.com>
2020-05-17 11:19:00 +0700

    edit rps.js

--- rps.js
+++ rps.js
@@ -1,28 +1,38 @@
+//Scores
+let playerScore = 0;
+let computerScore = 0;
+
+//Player Selecton
+const playerPlay = prompt("Rock, paper or Scissors").toLowerCase();
+
 //Compute Selection
 const computerPlay = () => {
-    let x = (Math.floor(Math.random() * 3))
+    let x = Math.floor(Math.random() * 3);
     if (x === 1) {
-        return 'rock';
+        return "rock";
     } else if (x === 2) {
-        return 'paper';
+        return "paper";
     } else {
-        return 'scissors';
+        return "scissors";
     }
-
 };
 
-console.log(computerPlay);
-
-
 //Play round
-function playRound(playerSelection, computerSelection) {}
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Tie!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return `Computer chooses ${computerSelection}. Player wins!`;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return `Computer chooses ${computerSelection}. Player wins!`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return `Computer chooses ${computerSelection}. Player wins!`;
+    } else {
+        return `Computer chooses ${computerSelection}. Computer Wins! Better luck next time!`;
+    }
+}
 
-
-/* Testing
-const playerSelection = 'rock
-const computerSelection = computerPLay()
-console.log(playRound(playerSelection, computerSelection))
-*/
+alert(playRound(playerPlay, computerPlay()));
 
 //Game runs round 5 times and declares a winner - using console.log to display results of each round
-function game()
+//function game()
