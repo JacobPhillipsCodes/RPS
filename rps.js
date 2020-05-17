2c9e2056a1bb4cf7968a0692334e3e8809d281c5
Jacob Phillips <jacobphillipscodes@gmail.com>
2020-05-17 09:08:40 +0700

    edit RPS

--- rps.js
+++ rps.js
@@ -1,1 +1,28 @@
-//I am a new file
+//Compute Selection
+const computerPlay = () => {
+    let x = (Math.floor(Math.random() * 3))
+    if (x === 1) {
+        return 'rock';
+    } else if (x === 2) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+
+};
+
+console.log(computerPlay);
+
+
+//Play round
+function playRound(playerSelection, computerSelection) {}
+
+
+/* Testing
+const playerSelection = 'rock
+const computerSelection = computerPLay()
+console.log(playRound(playerSelection, computerSelection))
+*/
+
+//Game runs round 5 times and declares a winner - using console.log to display results of each round
+function game()
